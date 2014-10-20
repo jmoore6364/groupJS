2f9e8c6e23d0530f8adc1cc5ce2ae38e6fedb2c0
jmoore6364 <jmoore6364@yahoo.com>
2014-10-19 18:52:19 -0700

    Added flatten functions
    These functions will take an array and flatten into a single object.

--- groupJS.js
+++ groupJS.js
@@ -6,14 +6,21 @@ var data = [
     { name: "Olivia", gender: "F", state: "FL", orders: 7 }
 ];
 
-var getMatches = function (data, newRow) {
+var match = function (data, newRow, props) {
     var result = [];
     for (var i = 0; i < data.length; i++) {
         var row = data[i];
         if (row.done)
             continue;
-        if (row.name == newRow.name &&
-            row.gender == newRow.gender) {
+        var isEqual = true;
+        for (var x = 0; x < props.length; x++) {
+            var prop = props[x]
+            if (row[prop] != newRow[prop]) {
+                isEqual = false;
+                break;
+            }
+        }
+        if (isEqual) {
             row.done = true;
             result.push(row);
         }
@@ -21,14 +28,55 @@ var getMatches = function (data, newRow) {
     return result;
 };
 
-var group = function (data) {
+var group = function (data, properties) {
     var result = []
     for (var i = 0; i < data.length; i++) {
         var row = data[i];
         if (row.done)
             continue;
-        var matches = getMatches(data, row);
+        var matches = match(data, row, properties);
         result.push(matches)
+    }
+    for (var i = 0; i < result.length; i++) {
+        for (var x = 0; x < result[i].length; x++) {
+            delete result[i][x].done;
+        }
     }
     return result;
 };
+
+var flatten = function (data, prop, sumProps) {
+    if (data.length == 0)
+        return {};
+    if (data.length == 1)
+        return data[0];
+    var result = JSON.parse(JSON.stringify(data[0]));
+    var propData = [];
+    propData.push(result[prop]);
+    for (var i = 1; i < data.length; i++) {
+        var row = data[i];
+        propData.push(row[prop]);
+        for (var x = 0; x < sumProps.length; x++) {
+            var sumProp = sumProps[x];
+            if (!row[sumProp])
+                continue;
+            if (!result[sumProp])
+                result[sumProp] = row[sumProp];
+            else
+                result[sumProp] += row[sumProp];
+        }
+    }
+    result[prop] = propData.join(',');
+    return result;
+};
+
+var flattenGroups = function (groups, prop, sumProps) {
+    var result = [];
+    for (var i = 0; i < groups.length; i++) {
+        result.push(flatten(groups[i], prop, sumProps));
+    }
+    return result;
+};
+
+var groups = group(flatData, ['name']);
+console.log(flattenGroups(groups, ['gender', 'state'], ['orders']));
