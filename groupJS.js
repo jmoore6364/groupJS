4af5c17794582b793ce2624027a018a8d6f56536
jmoore6364 <jmoore6364@yahoo.com>
2014-10-19 18:55:53 -0700

    Allow data matching within arrays
    Added some options to allow 2 arrays to match if any of the values have
    matches in the other.

--- groupJS.js
+++ groupJS.js
@@ -6,36 +6,67 @@ var data = [
     { name: "Olivia", gender: "F", state: "FL", orders: 7 }
 ];
 
-var match = function (data, newRow, props) {
+var isArray = function (myArray) {
+    return myArray.constructor.toString().indexOf("Array") > -1;
+}
+
+var anyOf = function (arrayIn, anyOfArray) {
+    for (var i = 0; i < anyOfArray.length; i++) {
+        var a = anyOfArray[i]
+        if (arrayIn.indexOf(a) > -1)
+            return true;
+    }
+    return false;
+};
+
+var isMatch = function (objA, objB, props, options) {
+    var isMatch = true;
+    for (var i = 0; i < props.length; i++) {
+        var prop = props[i];
+        var valueA = objA[prop];
+        var valueB = objB[prop];
+        if (isArray(valueA) && options && options.array && options.array == 'any') {
+            if (!anyOf(valueA, valueB))
+                return false;
+            else
+                continue;
+        }
+        else if (valueA != valueB)
+            return false;
+    }
+    return true;
+};
+
+var matches = function (data, obj, props, options) {
     var result = [];
     for (var i = 0; i < data.length; i++) {
         var row = data[i];
-        if (row.done)
-            continue;
-        var isEqual = true;
-        for (var x = 0; x < props.length; x++) {
-            var prop = props[x]
-            if (row[prop] != newRow[prop]) {
-                isEqual = false;
-                break;
-            }
-        }
-        if (isEqual) {
-            row.done = true;
+        if (isMatch(row, obj, props, options))
             result.push(row);
-        }
     }
     return result;
 };
 
-var group = function (data, properties) {
+var exists = function (data, value) {
+    for (var i = 0; i < data.length; i++) {
+        if (data[i] == value)
+            return true;
+    }
+    return false;
+};
+
+var group = function (data, properties, options) {
     var result = []
     for (var i = 0; i < data.length; i++) {
         var row = data[i];
-        if (row.done)
+        var done = false;
+        for (var x = 0; x < result.length; x++) {
+            if (exists(result[x], row))
+                done = true;
+        }
+        if (done)
             continue;
-        var matches = match(data, row, properties);
-        result.push(matches)
+        result.push(matches(data, row, properties, options))
     }
     for (var i = 0; i < result.length; i++) {
         for (var x = 0; x < result[i].length; x++) {
@@ -45,17 +76,23 @@ var group = function (data, properties) {
     return result;
 };
 
-var flatten = function (data, prop, sumProps) {
+var flatten = function (data, props, sumProps) {
     if (data.length == 0)
         return {};
     if (data.length == 1)
         return data[0];
     var result = JSON.parse(JSON.stringify(data[0]));
-    var propData = [];
-    propData.push(result[prop]);
+    var propData = getValues(result, props);
     for (var i = 1; i < data.length; i++) {
         var row = data[i];
-        propData.push(row[prop]);
+        for (var x = 0; x < props.length; x++) {
+            var prop = props[x];
+            var a = propData[prop];
+            var b = row[prop];
+            if (!exists(a, b))
+                a.push(b);
+        }
+
         for (var x = 0; x < sumProps.length; x++) {
             var sumProp = sumProps[x];
             if (!row[sumProp])
@@ -66,14 +103,26 @@ var flatten = function (data, prop, sumProps) {
                 result[sumProp] += row[sumProp];
         }
     }
-    result[prop] = propData.join(',');
+    for (var x = 0; x < props.length; x++) {
+        var prop = props[x];
+        result[prop] = propData[prop].join(',');
+    }
     return result;
 };
 
-var flattenGroups = function (groups, prop, sumProps) {
+var getValues = function (data, props) {
+    var result = {};
+    for (var i = 0; i < props.length; i++) {
+        var prop = props[i];
+        result[prop] = [data[prop]];
+    }
+    return result;
+};
+
+var flattenGroups = function (groups, props, sumProps) {
     var result = [];
     for (var i = 0; i < groups.length; i++) {
-        result.push(flatten(groups[i], prop, sumProps));
+        result.push(flatten(groups[i], props, sumProps));
     }
     return result;
 };
